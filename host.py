a4169f2bda16ec831f9c4dea54d31b0c7c0f09a7
80dropz <153262187+80dropz@users.noreply.github.com>
2024-10-29 14:01:22 -0400

    updated host.py allows for macOS auto ip config and better chat

--- host.py
+++ host.py
@@ -2,37 +2,59 @@
 import socket
 import random
 import threading
+import subprocess
+
+
+#basic grabbing the ip to host the server on local machine but the server is public
+
+
+#MacOS version
+iplist = subprocess.run(["ifconfig | grep 'inet ' | awk '{print $2}'"], capture_output=True, text=True, shell=True)
+iplines = iplist.stdout.splitlines()
+print(len(iplines))
+ip = iplines[1]
+ipconfig1 = iplines[0]
+print(ipconfig1)
+print(ip)
+
+#Windows version
 
 
 #Handles all the people connected to the server
 connected = []
 
 def handle_client(client_socket, addr):
-    print(f"Connected by {addr}")
+    print(f"Connection by {addr}")
     connected.append(client_socket)
-    while True:
-        try:
+    try:
+        while True:
             data = client_socket.recv(1024)
             if not data:
                 break
             print(f"Received from {addr}: {data.decode()}")
+
+            # Broadcast data to other clients
             for connection in connected:
                 if connection != client_socket:
                     try:
-                        connected.sendall(data)
+                        connection.sendall(data)
                     except BrokenPipeError:
-                        print(f"Connection with {addr} lost.")
-                        break
+                        print(f"Lost connection with a client.")
+                        connected.remove(connection)
                 
-            client_socket.sendall(data)
-        except ConnectionResetError:
-            print(f"Connection with {addr} lost.")
-            break
-    client_socket.close()
+        client_socket.close()
+    except ConnectionResetError:
+        print(f"Connection with {addr} lost.")
+    finally:
+        #ensures the client is removed from the connections list
+        connected.remove(client_socket)
+        client_socket.close()
+
+
 
 def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = '172.20.10.3'
+    host = ip
     port = 65432
     server_socket.bind((host, port))
     server_socket.listen()
